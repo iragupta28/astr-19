6685d5e07fa0e310e3b39fb46a313cea33318d4a
iragupta28 <irsgupta@ucsc.edu>
2024-10-01 11:02:59 -0700

    Added hello-world.py
    This repository is now less boring.

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+# This program will write "Hello World!"
+
+# This function tells the computer to print "Hello World!"
+def hello_world():
+	print("Hello World!")
+
+# This defines our main() function for our program.
+def main():
+	hello_world()
+
+# When we run the program, this executes first.
+if __name__=="__main__":
+	main()
