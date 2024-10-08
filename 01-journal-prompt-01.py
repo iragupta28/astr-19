3c4b36cb276f341b9e7b6f9846e8439e95489de0
iragupta28 <irsgupta@ucsc.edu>
2024-10-08 12:08:33 -0700

    Add files via upload

---
+++ 01-journal-prompt-01.py
@@ -0,0 +1,19 @@
+# Write a Python program to print out your full name, your preferred pronouns (optional), and two sentences about your favorite movie and your favorite food.
+
+def name():
+    print("My name is Ira Gupta.")
+    
+def pronouns():
+    print("My pronouns are she/her.")
+    
+def two_sentences():
+    print("My favorite movie is Interstellar. My favorite food is Indian food.")
+    
+# Defining the main function
+def main():
+    name()
+    pronouns()
+    two_sentences()
+    
+if __name__ == "__main__":
+    main()
